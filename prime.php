7894bc0649e832eabaf25b6e5c6031ffd30d127d
lizy-blip <123255129+lizy-blip@users.noreply.github.com>
2023-02-23 12:24:18 +0530

    Add files via upload

---
+++ prime.php
@@ -0,0 +1,17 @@
+<?php
+$MyNum = 21;
+$n = 0;
+
+for($i = 2; $i < $MyNum; $i++) {
+  if($MyNum % $i == 0){
+    $n++;
+    break;
+  }
+}
+
+if ($n == 0){
+  echo $MyNum." is a prime number.";
+} else {
+  echo $MyNum." is not a prime number.";
+}
+?>
---
+++ starpattern.php
@@ -0,0 +1,16 @@
+<?php
+    $size = 5;
+    
+    for ($i = 1; $i <= $size; $i++) {
+        for ($j = 0; $j < $i; $j++) {
+            echo "*";
+        }
+        echo "<br>";
+    }
+    for ($i = 1; $i <= $size - 1; $i++) {
+        for ($j = 0; $j < $size - $i; $j++) {
+            echo "*";
+        }
+        echo "<br>";
+    }
+?>
