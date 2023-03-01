f1c0c58208a95eb5f6c9e9e4b06ffb4f00d0ad0e
lizy-blip <123255129+lizy-blip@users.noreply.github.com>
2023-03-01 12:32:22 +0530

    Add files via upload

---
+++ numbers.php
@@ -0,0 +1,14 @@
+<?php  
+for($i=0;$j<=5;$i++){  
+for($i=1;$j<=$i;$j++){  
+echo $j;  
+}  
+echo "<br>";  
+}  
+for($i=0;$i<=5;$i++){  
+    for($j=5-$i;$j>=1;$j--){  
+    echo $j;  
+    }  
+    echo "<br>";  
+    }
+    ?>  
